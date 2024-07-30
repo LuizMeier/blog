70cc4f92d91fae2a95fcfbb47f3ba55987901e0d
Luiz Meier <lfmmeier@gmail.com>
2024-07-30 18:39:32 -0300

    Editing title

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,15 @@
 export const AppConfig = {
-	site_name: 'AstroPress',
-	title: 'AstroPress | Astro and Tailwind CSS',
-	description: 'Boilerplate built with Astro and Tailwind CSS',
-	author: 'Nicolas',
-	locale_region: 'de-en',
-	locale: 'en'
+	//site_name: 'AstroPress',
+	//title: 'AstroPress | Astro and Tailwind CSS',
+	//description: 'Boilerplate built with Astro and Tailwind CSS',
+	//author: 'Nicolas',
+	//locale_region: 'de-en',
+	//locale: 'en'
+
+	site_name: 'lmeier.net',
+	title: 'lmeier.net',
+	description: 'Luiz Meier blog',
+	author: 'Luiz',
+	locale_region: 'pr-br',
+	locale: 'br'
 };
